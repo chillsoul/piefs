f94c93546e6a81b6f99dd6f09cb8f5600c2d9bef
chillsoul <3213189@qq.com>
2022-03-25 11:16:56 +0800

    delete deprecated uuid

--- util/uuid.go
+++
@@ -1,9 +0,0 @@
-package util
-
-import "time"
-
-func UniqueId() (id uint64) {
-	//TODO 分布式唯一自增id snowflake
-	//Will return same uuid when high concurrent calls
-	return uint64(time.Now().UnixNano())
-}
